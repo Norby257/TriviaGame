e8cd405257a2c89e4e736a931cf0df8eb59c1ff3
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-16 09:39:20 -0600

    added pseudocode - conditional logic

--- javascript/app.js
+++ javascript/app.js
@@ -4,19 +4,35 @@
 //check if files are linked correctly 
 console.log("we are linked");
 
+//global variables to hold time and score
+
+var timer; 
+var counter; 
+
 //get document ready 
+$(document).on("ready", function() {
 
-// when start button is clicked 
-
-
-//Get timer to start counting down 
-
+    // when start button is clicked 
+    $("#start").on("click", function() {
+        //Get timer to start counting down 
+        $(".timer").addClass();
+        
 //while timer is greater than 0, 
 
 
-// dynamically update the dom to display questions and radio buttons 
+// dynamically update the DOM to display questions and radio buttons 
 
 //user can only select one radio button for each question
+///and then have a submit function to compare the value 
+//single comparison 
+
+//how to check if answers are correct? store correct answers and compare them 
+
+
+}
+
+
+
 
 
 
@@ -31,3 +47,14 @@ console.log("we are linked");
 //nice to have - picture of ron swanson pyramid of greatness if all answers are correct
 //here is the src for ron swanson https://vignette4.wikia.nocookie.net/parksandrecreation/images/3/3a/Swanson_Pyramid_of_Greatness.jpg/revision/latest?cb=20111014231319
 //if answers are not correct, display a jean ralphio meme here is source- https://uproxx.files.wordpress.com/2015/06/j-ralph-dizzity.gif?w=650
+
+
+
+
+
+
+
+
+
+
+});
