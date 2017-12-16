e466ba72b6b8c4501964d03e7f0390eb3139da1b
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-15 22:39:21 -0600

    Added initial functions

--- javascript/app.js
+++ javascript/app.js
@@ -14,20 +14,20 @@ console.log("we are linked");
 //while timer is greater than 0, 
 
 
-//display questions and radio buttons 
+// dynamically update the dom to display questions and radio buttons 
 
 //user can only select one radio button for each question
 
 
 
 
-/*once timer is zero, do the following:
--display all done!
---display correct answers: score 
--display incorrect answers: score
--display unanswered: score
+//once timer is zero, do the following:
+//display all done!
+//display correct answers: score 
+//display incorrect answers: score
+//display unanswered: score
 
---nice to have - sound effect if all answers are correct
---nice to have - picture of ron swanson pyramid of greatness if all answers are correct
---here is the src for ron swanson https://vignette4.wikia.nocookie.net/parksandrecreation/images/3/3a/Swanson_Pyramid_of_Greatness.jpg/revision/latest?cb=20111014231319
---if answers are not correct, display a jean ralphio meme here is source- https://uproxx.files.wordpress.com/2015/06/j-ralph-dizzity.gif?w=650
+//nice to have - sound effect if all answers are correct
+//nice to have - picture of ron swanson pyramid of greatness if all answers are correct
+//here is the src for ron swanson https://vignette4.wikia.nocookie.net/parksandrecreation/images/3/3a/Swanson_Pyramid_of_Greatness.jpg/revision/latest?cb=20111014231319
+//if answers are not correct, display a jean ralphio meme here is source- https://uproxx.files.wordpress.com/2015/06/j-ralph-dizzity.gif?w=650
