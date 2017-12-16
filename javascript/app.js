047f490677a98a2715cbc4d19351b67ef11ee164
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-15 21:23:15 -0600

    added pseudocode

---
+++ javascript/app.js
@@ -0,0 +1,31 @@
+
+//Pseudocode 
+
+//check if files are linked correctly 
+console.log("we are linked");
+
+//get document ready 
+
+// when start button is clicked 
+
+
+//Get timer to start counting down 
+
+//while timer is greater than 0, 
+
+
+//display questions and radio buttons 
+
+//user can only select one radio button for each question
+
+
+
+
+/*once timer is zero, do the following:
+-display all done!
+--display correct answers: score 
+-display incorrect answers: score
+-display unanswered: score
+
+--nice to have - sound effect if all answers are correct
+--nice to have - picture of ron swanson 
