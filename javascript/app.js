cff7f65c02b64eb3e365e1bd9b6aca20036ead2c
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-24 12:23:32 -0600

    removed extraneous pseudocode

--- javascript/app.js
+++ javascript/app.js
@@ -7,9 +7,6 @@ console.log("we are linked");
 //global variables to hold time and score
 
 //you know what would be cool? randomly displaying a new set of questions every time the start button is pressed 
-//API calls to a tv show database 
-
-
 
 //get document ready 
 $(document).ready(function() {
@@ -25,20 +22,13 @@ $(document).ready(function() {
             var questionsNotAnswered;
             $(".timer").html("<h1> " + duration + "</h1>");
             $(".questions").hide();
+            $("#ron").hide();
+            $("#ralphio").hide();
 
             //hide answers  http://api.jquery.com/val/
             //build the radio buttons in the html then hide them. when button is clicked show them 
             //use val attribute to get val of answer. compare to correct answer to score 
-
-             //set timer to 1:20 on page 
-        // $(".timer").text("1:20");
-        // //Get timer to start counting down 
-        // function countDown() {
-        //     //count down one second at a time 
-        //     invervalId = setInterval(decrement, 1000);
-            //show the questions 
-            //disable the button after it's clicked 
-
+ 
             
     //user clicks button, questions and answers are displayed 
     $("#start").click(function() {
@@ -46,10 +36,6 @@ $(document).ready(function() {
         // $(".choices").show();
         console.log("Yay, you started the game");
         $(".questions").show();
-        //keep score
-        //get value of each question. if matches correct value. then add one to counter
-        $( "input[type=radio]:checked" ).val();
-        
 
       //var for timer. when button is clicked, the timer starts DECREMENTING 
         var intervalId = setInterval(function() {
@@ -60,15 +46,33 @@ $(document).ready(function() {
                 alert("the game is over!");
                 clearInterval(intervalId);
                 $(".questions").empty();
-                $("<p>").text(`Correct Answers: ${score}`);
-                $("<p>").text(`Incorrect Answers: ${score}`);
-                $("<p>").text(`unanswered: ${score}`);
-            
-                
-    
+               
                 $("#start").prop("disabled", false);
                 duration = 5;
-                $(".timer").html("<h1> " + duration + "</h1>");            
+                $(".timer").html("<h1> " + duration + "</h1>");    
+                
+                //function for showing answers -pseudo code since I could not finish this part 
+                function showScore(score) {
+                    $( "input[type=radio]:checked" ).val();
+                    $("<p>").text(`Correct Answers: ${score}`);
+                    $("<p>").text(`Incorrect Answers: ${score}`);
+                    $("<p>").text(`unanswered: ${score}`);
+                    //checking to see which text + image to display to the user 
+                    if (score === 4) {
+                        // text if you win 
+                        alert("Victory! Ron Swanson is proud.");
+                        // image if you win 
+                        $("#ron").show();
+                        
+                    } else  {
+                        //text if you lose 
+                        alert("Try again! Sometimes you have to work a litle, so you can ball a lot!");
+                        //image if you lose 
+                        $("#ralphio").show();
+                        
+                    }
+                    
+                }
                 
                 
             }
@@ -86,18 +90,8 @@ $(document).ready(function() {
 });
 
 
-//once timer is zero, do the following:
-
-
-
-//here is the src for ron swanson https://vignette4.wikia.nocookie.net/parksandrecreation/images/3/3a/Swanson_Pyramid_of_Greatness.jpg/revision/latest?cb=20111014231319
-//if answers are not correct, display a jean ralphio meme here is source- https://uproxx.files.wordpress.com/2015/06/j-ralph-dizzity.gif?w=650
-
-
-/* here are the API's I'll be using 
--- OPEN API TRIVIA - https://opentdb.com/api.php?amount=10&category=14&difficulty=medium&type=multiple
--- GIPHY API 
-
+/* to make this better in the future, it'd be more fun to dynamically update the DOM with an API call to GIPHY for win /lose random gif
+to make this better in the future, it'd be more fun to pull randomly generated Parks and Recreation questions/answers each game
 
 
 */ 
